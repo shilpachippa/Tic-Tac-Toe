551d3121093839a9cb15400a61bae8a149e71a0a
shilpachippa <166416956+shilpachippa@users.noreply.github.com>
2025-05-16 20:13:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+console.log("welome to Tic Tac Toe");
+let music = new Audio("music.mp3");
+let audioTurn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let turn = "x";
+let gamecom = false;
+
+//Function to change the turn
+ const changeTurn = ()=>{
+  return turn === "x" ? "0": "x"
+ }
+
+
+  //Function to check for win
+ const checkWin = ()=>{
+  let boxtext = document.getElementsByClassName('boxtext');
+  let wins = [
+    [0,1,2,5,5,0],
+    [3,4,5,5,15,0],
+    [6,7,8,5,25,0],
+    [0,3,6,-5,5,90],
+    [1,4,7,5,15,90],
+    [2,5,8,15,15,90],
+    [0,4,8,5,15,45],
+    [2,4,6,5,15,135]
+  ]
+  wins.forEach(e =>{
+    if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !="") ){
+      document.querySelector('.info').innerText = boxtext[e[0]].innerText + "Won";
+      gamecom = true;
+      document.querySelector(".imgbox").getElementsByTagName('img')[0].style.width = "200px";
+      document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+      document.querySelector(".line").style.width = "20vw";
+    }
+  })
+}
+
+
+//Game logic
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+  let boxtext = element.querySelector('.boxtext');
+  element.addEventListener('click',() =>{
+    if(boxtext.innerText === ""){
+      boxtext.innerText = turn;
+      turn = changeTurn();
+      audioTurn.play();
+      checkWin();
+      if(!gamecom)
+        {
+          document.querySelector(".info").innerText = "Turn for " + turn;
+        }
+    
+    }
+  });
+});
+
+//add a  onclick listener to reset the button
+reset.addEventListener('click',() => {
+  let boxtext = document.querySelectorAll('.boxtext');
+  boxtext.forEach(element => {
+    element.innerText = "";
+  })
+  turn ="x";
+  gamecom = false;
+  document.querySelector(".line").style.width = "0vw";
+  document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
+  document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+})
